e1656d5cb4cc64ae1b6f92f69873ad73f37b82c0
Ankon <44019225+luciferankon@users.noreply.github.com>
2019-04-16 10:48:17 +0530

    implemented search feature

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
+const view = require("ejs");
 
 const connection = mysql.createConnection({
   host: "us-cdbr-iron-east-02.cleardb.net",
@@ -13,46 +14,72 @@ connection.connect();
 
 const app = express();
 
+app.set("views", __dirname + "/instagram_react/build");
+app.engine("html", view.renderFile);
+app.set("view engine", "html");
+
 app.use(express.static("instagram_react/build"));
+app.get(/\/user/, (req, res) => {
+  res.render("index.html");
+});
+
+app.use((req, res) => {
+  console.log(req.url);
+  req.next();
+});
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const makeQuery = function(res, query) {
   connection.query(query, (error, results, fields) => {
+    if (error) console.log(error);
     res.send(JSON.stringify(results[0]));
   });
 };
 
-app.get("/profile/name", (req, res) => {
-  makeQuery(res, "select username as userName from users limit 1");
+app.post("/profile/name", (req, res) => {
+  const name = req.body;
+  res.send(JSON.stringify({ userName: name }));
 });
 
-app.get("/profile/follower", (req, res) => {
+app.post("/profile/follower", (req, res) => {
+  const name = req.body;
   makeQuery(
     res,
-    "select count(follower_id ) as follower from follow where followee_id =1"
+    `select count(follower_id) as follower from follow where followee_id=(select id from users where username="${name}")`
   );
 });
 
-app.get("/profile/followee", (req, res) => {
+app.post("/profile/followee", (req, res) => {
+  const name = req.body;
   makeQuery(
     res,
-    "select count(followee_id ) as followee from follow where follower_id =1"
+    `select count(followee_id) as followee from follow where follower_id=(select id from users where username="${name}")`
   );
 });
 
-app.get("/profile/post", (req, res) => {
-  makeQuery(res, "select count(photo_url) as post from photos where user_id=1");
+app.post("/profile/post", (req, res) => {
+  const name = req.body;
+  makeQuery(
+    res,
+    `select count(photo_url) as post from photos where user_id=(select id from users where username="${name}")`
+  );
 });
 
-app.get("/profile/posts", (req, res) => {
+app.post("/profile/posts", (req, res) => {
+  const name = req.body;
   connection.query(
-    "select photo_url from photos where user_id=1;",
+    `select photo_url from photos where user_id=(select id from users where username="${name}")`,
     (err, results, fields) => {
       const posts = results.map(result => result.photo_url);
       res.send(JSON.stringify({ posts }));
     }
   );
+});
+
+app.post("/search", (req, res) => {
+  const name = req.body.name;
+  res.redirect(`/user?name=${name}`);
 });
 
 app.listen(process.env.PORT || 8000);
